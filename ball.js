46832a9a54d86cbad7ce195243a0621a6af9af4f
Hamish Kerr <user02@gmail.com>
2017-12-03 15:10:41 -0800

    remove stale balls, neaten up code

---
+++ ball.js
@@ -0,0 +1,51 @@
+class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.diameter = 20;
+    this.color = [random(127,255),random(0,255),random(127,255)];
+    this.alpha = 255;
+    this.xVel = random(-5,5); // ball starts with random x velocity
+    this.yVel = -10; //random(0,-10); // ball starts with random y velocity
+  }
+
+  refresh() {
+    this.refreshX();
+    this.refreshY();
+    this.fadeOnFloor();
+  }
+  
+  refreshX() {
+    this.x = this.x + this.xVel;
+    if (this.x >= canvasWidth || this.x <= 0) {
+      this.xVel = -restituion * this.xVel; // bounce when hits a wall
+    }
+  }
+
+  refreshY() {
+    this.yVel = this.yVel + gravity; // update y velocity based on gravity
+    if (this.yVel >= terminalVelocity) {
+      this.yVel = terminalVelocity; // limit terminal velocity
+    }
+    this.y = this.y + this.yVel; // update position based on velocity
+    if (this.y > canvasHeight) {
+      this.y = canvasHeight; // don't let the ball go outside the screen limits
+    }
+    if (this.y >= canvasHeight) {
+      this.yVel = -restituion * this.yVel; // bounce when hits edge of screen 
+    }
+  }
+
+  fadeOnFloor() {
+    if (this.y == canvasHeight) {
+      this.alpha--;
+    }
+  }
+
+  show() {
+    stroke(this.color[0],this.color[1], this.color[2], this.alpha);
+    noFill();
+    strokeWeight(4);
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -22,9 +22,13 @@ function draw() {
 }
 
 function updateBalls() {
-  for (let i = 0; i < balls.length; i ++) {
-    balls[i].refresh();
-    balls[i].show();
+  for (let i = balls.length -1; i >= 0; i--) {
+    balls[i].refresh(); // update ball positions
+    if (balls[i].alpha <= 0) {
+      balls.splice(i,1); // remove invisible balls
+    } else {
+      balls[i].show(); //render all balls
+    }
   }
 }
 
@@ -49,48 +53,5 @@ function resizeIfNeeded() {
     canvasHeight = window.innerHeight;
     resizeCanvas(canvasWidth,canvasHeight);
     console.log('resizing');
-  }
-}
-
-class Ball {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.diameter = 20;
-    this.color = [random(127,255),random(0,255),random(127,255)]; //
-    this.xVel = random(-5,5); // ball starts with random x velocity
-    this.yVel = -10; //random(0,-10); // ball starts with random y velocity
-  }
-
-  refresh() {
-    this.refreshX();
-    this.refreshY();
-  }
-  
-  refreshX() {
-    this.x = this.x + this.xVel;
-    if (this.x >= canvasWidth || this.x <= 0) {
-      this.xVel = -restituion * this.xVel; // bounce when hits a wall
-    }
-  }
-
-  refreshY() {
-    this.yVel = this.yVel + gravity; // update y velocity based on gravity
-    if (this.yVel >= terminalVelocity) {
-      this.yVel = terminalVelocity; // limit terminal velocity
-    }
-    this.y = this.y + this.yVel; // update position based on velocity
-    if (this.y > canvasHeight) {
-      this.y = canvasHeight; // don't let the ball go outside the screen limits
-    }
-    if (this.y >= canvasHeight) {
-      this.yVel = -restituion * this.yVel; // bounce when hits edge of screen 
-    }
-  }
-  show() {
-    stroke(this.color);
-    noFill();
-    strokeWeight(4);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
