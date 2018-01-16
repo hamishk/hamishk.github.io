81c8ff9a16e126a6fc0b1955107c958c51a6d36f
Hamish Kerr <user02@gmail.com>
2018-01-15 19:07:34 -0800

    finish cosmos v1.0

--- cosmos/sketch.js
+++ cosmos/sketch.js
@@ -1,8 +1,8 @@
 let canvasWidth = window.innerWidth; 
-let canvasHeight = window.innerHeight - 20;
+let canvasHeight = window.innerHeight - 30;
 let dots = [];
-let dotradius = 3;
-let dotpacity = 15;
+let minradius = 2;
+let minpacity = 15;
 let dotdensity = 6.5 // arbitrary scale of [dots / (x * y)] *10,000  
 let dotcount = canvasWidth * canvasHeight * dotdensity / 10000;
 let sliders =[]; // need access as global
@@ -15,17 +15,19 @@ function setup() {
   }
   noStroke();
   background(bgCol); //don't redraw background
-  sliders[0] = createSlider(0,255,dotradius);
-  sliders[1] = createSlider(0,255,dotpacity);
+  sliders[0] = createSlider(0,10,minradius);
+  sliders[1] = createSlider(0,255,minpacity);
 }
 
 function draw() {
-  dotradius = sliders[0].value();
-  dotpacity = sliders[1].value();
+  minradius = sliders[0].value();
+  minpacity = sliders[1].value();
   for (let i = 0; i < dots.length; i++) {
-    // fill(random(255), dotpacity);
-    fill(random(255), max(dotpacity, random(-10000,255))); // allow for rare bright stars
-    ellipse(dots[i].x, dots[i].y, dotradius, dotradius);
+    // fill(random(255), minpacity);
+    let opacity = max(minpacity, random(-10000,255));
+    fill(random(255), opacity); // allow for rare bright stars
+    let radius = max(minradius, random(-50,5));
+    ellipse(dots[i].x, dots[i].y, radius, radius);
     dots[i].update();
   }
 }
